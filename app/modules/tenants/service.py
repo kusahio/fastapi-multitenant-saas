af07c4f70f60188e5e03059e56767894dd2d1833
CamiloIllanes <67984533+kusahio@users.noreply.github.com>
2026-02-18 13:42:40 -0300

    Remover configuración de VS Code del repositorio

--- app/modules/tenants/service.py
+++ app/modules/tenants/service.py
@@ -1,8 +1,8 @@
 from sqlalchemy.orm import Session
 from app.modules.tenants.repository import TenantRepository
-from app.modules.tenants.schemas import TenantCreate
+from app.modules.tenants.schemas import TenantCreate, TenantUpdate, TenantRead
 from app.modules.tenants.models import Tenant
-from app.domain.errors.tenant import TenantAlreadyExistsError
+from app.domain.errors.tenant import TenantAlreadyExistsError, TenantNotFoundError
 
 class TenantService:
     def __init__(self, tenant_repository: TenantRepository):
@@ -17,3 +17,30 @@ class TenantService:
       tenant = Tenant(**data.model_dump(), active=True)
 
       return self.tenant_repository.save(db, tenant)
+    
+    def get_by_id(self, db: Session, tenant_id: int) -> Tenant:
+      tenant = self.tenant_repository.get_by_id(db, tenant_id)
+
+      if not tenant:
+        raise TenantNotFoundError()
+      
+      return tenant
+
+    def update(self, db: Session, tenant_id: int, data: TenantUpdate) -> Tenant:
+      tenant = self.get_by_id(db, tenant_id)
+      updated_data = data.model_dump(exclude_unset=True)
+
+      return self.tenant_repository.update(db, tenant, updated_data)
+    
+    def get_by_slug(self, db: Session, tenant_slug: str) -> Tenant:
+      tenant = self.tenant_repository.get_by_slug(db, tenant_slug)
+
+      if not tenant:
+        raise TenantNotFoundError()
+      
+      return tenant
+    
+    def get_list(self, db: Session) -> list[Tenant]:
+      tenants = self.tenant_repository.list(db)
+
+      return tenants
