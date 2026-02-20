104764b16a1638066ac167c459f0e6c7cc5477fa
CamiloIllanes <67984533+kusahio@users.noreply.github.com>
2026-02-19 23:58:48 -0300

    routers de tenants configurados

--- app/modules/tenants/router.py
+++ app/modules/tenants/router.py
@@ -8,9 +8,98 @@ from app.domain.errors.tenant import TenantAlreadyExistsError, TenantNotFoundErr
 
 router = APIRouter(prefix='/tenants', tags=['tenants'])
 
-@router.post('/', status_code=status.HTTP_201_CREATED)
-def create_tenant(tenant: TenantCreate, service: TenantService, db: Session = Depends(get_db)):
+def get_tenant_service() -> TenantService:
+    repository = TenantRepository()
+    return TenantService(repository)
 
-  new_tenant = service.create(db, tenant)
+@router.post('/', response_model=TenantRead, status_code=status.HTTP_201_CREATED)
+def create_tenant(
+  tenant: TenantCreate,
+  db: Session = Depends(get_db),
+  service: TenantService = Depends(get_tenant_service)):
 
-  return new_tenant
+  try:
+    return service.create(db, tenant)
+  except TenantAlreadyExistsError:
+    raise HTTPException(
+      status_code=status.HTTP_409_CONFLICT,
+      detail='Tenant with this slug already exists'
+    )
+
+@router.get('/', response_model=list[TenantRead], status_code=status.HTTP_200_OK)
+def list_tenants(
+  db: Session = Depends(get_db), 
+  service: TenantService = Depends(get_tenant_service)
+):
+  return service.get_list(db)
+
+@router.get('/tenant/{tenant_id}', response_model=TenantRead, status_code=status.HTTP_200_OK)
+def get_tenant_by_id(
+  tenant_id: int,
+  db: Session = Depends(get_db), 
+  service: TenantService = Depends(get_tenant_service)
+):
+  try:
+    return service.get_by_id(db, tenant_id)
+  except TenantNotFoundError:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail='Tenant not found'
+    )
+
+@router.get('/tenant/{tenant_slug}', response_model=TenantRead, status_code=status.HTTP_200_OK)
+def get_tenant_by_slug(
+  tenant_slug: str,
+  db: Session = Depends(get_db),
+  service: TenantService = Depends(get_tenant_service)
+):
+  try:
+    return service.get_by_slug(db, tenant_slug)
+  except TenantNotFoundError:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail='Tenant not found'
+    )
+  
+@router.put('/tenant/{tenant_id}', response_model=TenantRead, status_code=status.HTTP_201_CREATED)
+def update_tenant(
+  tenant_id: int,
+  data: TenantUpdate,
+  db: Session = Depends(get_db),
+  service: TenantService = Depends(get_tenant_service)
+):
+  try:
+    return service.update(db, tenant_id, data)
+  except TenantNotFoundError:
+    raise HTTPException(
+      status.HTTP_404_NOT_FOUND,
+      detail='Tenant not found'
+    )
+
+@router.patch('tenant/{tenant_id}/activate', response_model=TenantRead, status_code=status.HTTP_200_OK)
+def activate_tenant(
+  tenant_id: int,
+  db: Session = Depends(get_db),
+  service: TenantService = Depends(get_tenant_service)
+  ):
+  try:
+    return service.update(db, tenant_id, TenantUpdate(active=True))
+  except TenantNotFoundError:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail='Tenant not found'
+    )
+
+@router.patch('tenant/{tenant_id}/deactivate', response_model=TenantRead, status_code=status.HTTP_200_OK)
+def deactivate_tenant(
+  tenant_id: int,
+  db: Session = Depends(get_db),
+  service: TenantService = Depends(get_tenant_service)
+  ):
+  try:
+    return service.update(db, tenant_id, TenantUpdate(active=False))
+  except TenantNotFoundError:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail='Tenant not found'
+    )
